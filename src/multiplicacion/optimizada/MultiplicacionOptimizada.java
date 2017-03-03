1b61bad11128a9519e85250b77fb9f9f629bde45
ander <ander@DESKTOP-UGVN2VD>
2017-03-03 11:53:03 -0500

    optimizado mas o menos y funcional

--- src/multiplicacion/optimizada/MultiplicacionOptimizada.java
+++ src/multiplicacion/optimizada/MultiplicacionOptimizada.java
@@ -21,23 +21,21 @@ public class MultiplicacionOptimizada {
                 byte filas2= sc.nextByte();
                 System.out.print("Digite numero de columnas de la matriz 2:");
                 byte columnas2= sc.nextByte();
-
                 System.out.println();
                 if(columnas1==filas2){
-                    
-                llenadoMatriz1(filas1, columnas1);
-                llenadoMatriz2(filas2, columnas2);
-                multiplicacionEImpresion(filas1, columnas1, columnas2);
+                    //creacion de matrices
+                    byte [][] matriz1 = new byte [filas1][columnas1];
+                    byte [][] matriz2 = new byte [filas2][columnas2];
+                    llenadoMatrices(matriz1, matriz2);
+                  
                 }else
                 System.out.println("El programa no puede multiplicar las matrices (Numero de filas1 debe ser igual a columnas2) ");      
         }
-        public static void  llenadoMatriz1(byte filas01, byte columnas01){
+        public static void  llenadoMatrices(byte matriz1[][], byte matriz2[][]){
             Scanner sc = new Scanner(System.in);
-            //creacion de matrices
-            byte [][] matriz1 = new byte [filas01][columnas01];
             System.out.println("Llenado de matriz 1");
-            for(int i=0; i<filas01; i++){
-                for(int j=0; j<columnas01; j++ ){
+            for(int i=0; i<matriz1.length; i++){
+                for(int j=0; j<matriz1[i].length; j++ ){
                     System.out.print("Digite el valor para la posicion"+ i+ j +": " );
                     matriz1[i][j] = sc.nextByte();
                  }
@@ -45,21 +43,16 @@ public class MultiplicacionOptimizada {
             }
             System.out.println("Matriz 1:"); 
             //Se imprime la matriz1
-            for(int i=0; i<filas01; i++){
-                 for(int j=0; j<columnas01; j++){
+            for(int i=0; i<matriz1.length; i++){
+                 for(int j=0; j<matriz1[i].length; j++){
                   System.out.print(matriz1 [i][j]+ " ");
                 }
             System.out.println();     
-            }     
-        }
-        public static void llenadoMatriz2(byte filas02, byte columnas02){
-            Scanner sc = new Scanner(System.in);
-            byte [][] matriz2 = new byte [filas02][columnas02];
-            System.out.println();  
+            }      
             //Se llena la matriz2
             System.out.println("Llenado de matriz 2");
-            for(int i=0; i<filas02; i++){
-                for(int j=0; j<columnas02; j++ ){
+            for(int i=0; i<matriz2.length; i++){
+                for(int j=0; j<matriz2[i].length; j++ ){
                     System.out.print("Digite el valor para la posicion"+ i+ j +": " );
                     matriz2[i][j] = sc.nextByte();
                 }
@@ -67,28 +60,29 @@ public class MultiplicacionOptimizada {
             }
             System.out.println("Matriz 2:");
             //Se imprime la matriz2
-            for(int i=0; i<filas02; i++){
-                for(int j=0; j<columnas02; j++){
+            for(int i=0; i<matriz2.length; i++){
+                for(int j=0; j<matriz2[i].length; j++){
                     System.out.print(matriz2 [i][j] +" ");
                 }
                 System.out.println();
             }
-             System.out.println();
+            System.out.println();
+            multiplicacionEImpresion(matriz1, matriz2);
         }
         //multiplicacion de matrices
-        public static void multiplicacionEImpresion(byte filas01, byte columnas01, byte columnas02){
-        byte [][] matrizResult = new byte [filas01][columnas02];
-        for(int i=0; i<filas01; i++){
-            for(int j=0; j<columnas02; j++){
-                for(int k=0; k<columnas01; k++){ 
-                    matrizResult[i][j]=matrizResult[i][j]+(matriz01[i][k]*matriz02[k][j]);
+        public static void multiplicacionEImpresion(byte matriz1[][], byte matriz2[][]){
+        byte [][] matrizResult = new byte [matriz1.length][matriz2[0].length];
+        for(int i=0; i<matrizResult.length; i++){
+            for(int j=0; j<matrizResult[0].length; j++){
+                for(int k=0; k<matriz2.length; k++){ 
+                    matrizResult[i][j]+=(matriz1[i][k]*matriz2[k][j]);
                 }
             }
         }
         //Impresion de la matriz resultado
         System.out.println("Matriz Resultado:");
-         for(int i=0; i<filas01; i++){
-            for(int j=0; j<columnas02; j++){
+         for(int i=0; i<matrizResult.length; i++){
+            for(int j=0; j<matriz2[i].length; j++){
                 System.out.print(matrizResult [i][j]+ " ");
             }
             System.out.println();
